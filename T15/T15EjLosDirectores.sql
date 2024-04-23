731bfc6e54c63fa47d30960576bd26699761fca9
Joel García Pérez <perezjoelgarcia13@gmail.com>
2024-04-23 13:44:02 +0200

    T15 y cambios en el 14

---
+++ T15/T15EjLosDirectores.sql
@@ -0,0 +1,14 @@
+INSERT INTRO despachos (capacidad) VALUES
+(1) (2) (3) (4) (5) (6) (7) (8) (9) (10)
+
+INSERT INTO directores (dni, nom_apels, dni_jefe, num_despacho) VALUES
+('12345', 'CEE ','Central ', NULL, '1'), 
+('22345', 'Dwayne', 'Jahseh', NULL, '2'), 
+('32345', 'Ward', 'Micheal', NULL, '3'), 
+('42345', 'García', 'Joel', NULL, '4'), 
+('62345', 'Myers', 'Bryant', NULL, '5'), 
+('52345', 'Orobosa', 'David', NULL, '6'), 
+('72345', 'Bellingham', 'Jude', NULL, '7'), 
+('82345', 'Foden', 'Phil', NULL, '8'), 
+('92345', 'Tormo', 'Marc', NULL, '9'), 
+('13345', 'García', 'Carlos Vicente', NULL, '10');
