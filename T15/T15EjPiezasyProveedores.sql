3bb495a2d626ac97e7bea333f901ae9c99775d52
Joel García Pérez <perezjoelgarcia13@gmail.com>
2024-05-06 10:02:03 +0200

    Update T15EjPiezasyProveedores.sql

--- T15/T15EjPiezasyProveedores.sql
+++ T15/T15EjPiezasyProveedores.sql
@@ -1,4 +1,5 @@
 INSERT INTO piezas (codPieza) VALUES 
+(1), (2), (3), (4), (5), (6), (7), (8), (9), (10);
 
 INSERT INTO proveedores (idProveedor) VALUES 
 (1), (2), (3), (4), (5), (6), (7), (8), (9), (10);
